40184ed2bbbfb745da253a599bb8bc32047affa9
Takaesu._.p <zinn.1019t@gmail.com>
2026-09-21 12:23:36 +0000

    fix: 互換性エラーを防ぐため sitemap インテグレーションを一旦削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tsu.stki.org',
-  integrations: [
-    sitemap({
-      // サイトマップのオプションを明示してエラーを防ぐ
-      customPages: ['https://tsu.stki.org/'],
-    }),
-  ],
 });
