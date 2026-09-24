a6a55c24e374ba6459110ca3e8d913657fb6df0c
Takaesu._.p <zinn.1019t@gmail.com>
2026-09-24 11:37:17 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'; // 1. インポートを追加
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tsu.stki.org',
+  integrations: [sitemap()], // 2. integrations に追加
 });
